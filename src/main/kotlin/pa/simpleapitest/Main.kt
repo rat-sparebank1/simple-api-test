00f2c93aad98eecdf013eb8625e41c1082143cdd
Rafael Tello <rafael.tello@sparebank1.no>
2024-08-23 07:44:53 +0200

    fix dependency problem and change repo structure

--- src/main/kotlin/pa/simpleapitest/Main.kt
+++ src/main/kotlin/pa/simpleapitest/Main.kt
@@ -3,7 +3,7 @@ package pa.simpleapitest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["pa.simpleapitest.biz"])
+@SpringBootApplication(scanBasePackages = ["pa.simpleapitest.biz", "pa.simpleapitest.rest"])
 open class BookApiApplication
 
 fun main(args: Array<String>) {
